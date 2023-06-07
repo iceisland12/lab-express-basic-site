1f2b2a24204de622ff053debc57ada78c6debcbd
Luis <luisguile495@gmail.com>
2023-06-07 10:34:39 -0400

    Creada Home, about

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/works', (req,res) => {
+    res.sendFile(__dirname + '/views/work.html');
+});
+
+app.get('/gallery', (req,res) => {
+    res.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(3000,() =>{
+    console.log('Si Funciona el server! 😁😁')
+})
